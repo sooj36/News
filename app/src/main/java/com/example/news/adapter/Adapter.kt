db668bc8df6b1fe754113943df21ca8ee6270aa9
sooj36 <sooj2023@gmail.com>
2023-09-16 23:51:02 +0900

    [FEAT] : ⚙️ | Adapter.kt

--- app/src/main/java/com/example/news/adapter/Adapter.kt
+++ app/src/main/java/com/example/news/adapter/Adapter.kt
@@ -11,31 +11,19 @@ import com.example.news.R
 class Adapter(
     private val context: Context,
     private val list: Array<String>,
+    private val  itemClickListener: (String) -> Unit
 ) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     interface OnItemClickListener { //list 클릭 시 수행할
-        fun onItemClick(article : String)
+        fun onItemClick(article: String)
     }
 
-    private var listener : OnItemClickListener? = null
+    private var listener: OnItemClickListener? = null
 
     fun setOnItemClickListener(listener: OnItemClickListener) {
         this.listener = listener
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        private val textView : TextView = itemView.findViewById(R.id.news_text_view)
-
-        fun bind(news : String) {
-            textView.text = news
-
-            // 아이템 클릭 -> listener 로 클릭된 아이템 (news : String) 전달
-            itemView.setOnClickListener {
-                listener?.onItemClick(news)
-            }
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.news_list_item, parent, false)
@@ -48,4 +36,18 @@ class Adapter(
     }
 
     override fun getItemCount() = list.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val textView: TextView = itemView.findViewById(R.id.news_text_view)
+
+        fun bind(news: String) {
+            textView.text = news
+
+            // 아이템 클릭 -> listener 로 클릭된 아이템 (news : String) 전달
+            itemView.setOnClickListener {
+                itemClickListener(news)
+            }
+        }
     }
+}
